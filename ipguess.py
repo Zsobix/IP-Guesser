f7a31c49498ff43813cc64638bcb1608bb4f9718
Zsobix <45757909+Zsobix@users.noreply.github.com>
2021-12-07 20:52:43 +0100

    Add files via upload

--- ipguess.py
+++ ipguess.py
@@ -1,47 +1,45 @@
 import os
 import random
-import time
 import urllib3
 
-nonce = 1
+if os.name != "posix":
+	exit()
+
+http = urllib3.PoolManager()
+
+def log(watlog):
+	asdasd = open('ips.txt', 'a')
+	asdasd.write(watlog)
+	asdasd.close()
+
 while True:
-    a = open("ips.txt", 'r')
-    data = a.read()
-    point = "."
-    space = " "
-    ipnum1 = random.randint(0, 255)
-    ipnum2 = random.randint(0, 255)
-    ipnum3 = random.randint(0, 255)
-    ipnum4 = random.randint(0, 255)
-    ipnum1 = str(ipnum1)
-    ipnum2 = str(ipnum2)
-    ipnum3 = str(ipnum3)
-    ipnum4 = str(ipnum4)
-    fullip = ipnum1 + point + ipnum2 + point + ipnum3 + point + ipnum4
-    fullip = str(fullip)
-    url = urllib3.PoolManager()
-    a = url.request('GET', 'http://ip-api.com/csv/' + fullip + '?fields=country,city')
-    cc = a.data
-    print("a")
-    if cc == b'' or '' or b'\n' or b'/n' or None:
-        a.close()
-        os.system("python3 ipguess.py")
-        exit()
-    print("A IP is guessed")
-    fullip = fullip + space
-    nonce + nonce+1
-    print(fullip)
-    print(cc)
-    if fullip in data:
-        a.close()
-        time.sleep(1)
-        os.system("python3 ipguess.py")
-        exit()
-    save = open("ips.txt", 'a')
-    save.write(fullip)
-    save.close()
-    nonce = nonce
-    if nonce == 340282366920938463463374607431768211456:
-        a.close()
-        print("Done")
-        
+	openips = open('ips.txt', 'r')
+	asd = openips.read()
+	ipnum1 = random.randint(0, 255)
+	ipnum2 = random.randint(0, 255)
+	ipnum3 = random.randint(0, 255)
+	ipnum4 = random.randint(0, 255)
+	ipnum1 = str(ipnum1)
+	ipnum2 = str(ipnum2)
+	ipnum3 = str(ipnum3)
+	ipnum4 = str(ipnum4)
+	ip = ipnum1 + "." + ipnum2 + "." + ipnum3 + "." + ipnum4
+	if ip in asd:
+		os.system("python3 ipguess.py")
+		openips.close()
+		exit()
+	r = http.request('GET', 'http://ip-api.com/csv/' + ip + '?fields=country,city')
+	response = r.data
+	response = response.decode(encoding='UTF-8')
+	if response == "":
+		print('a')
+		os.system("python3 ipguess.py")
+		openips.close()
+		exit()
+	print(ip)
+	print(response)
+	ip = ip + '\n'
+	log(ip)
+	openips.close()
+
+
