9a4aafb4a8f53fcc8be0685cc5828bde1bda2c8a
Zsobix <index@zsobix.xyz>
2022-01-24 11:42:39 +0100

    Move To IP-Info

--- main.py
+++ main.py
@@ -5,13 +5,16 @@ from os import system as terminal
 import random
 import urllib3
 import time
+import base64
+import ipinfo
 
+acc_token_b64 = "ODgyZmIwYTNjNTIwZTQ="
+acc_token_bytes = base64.b64decode(acc_token_b64)
+acc_token = str(acc_token_bytes, "utf-8")
 ipcount = 0
-
+handler = ipinfo.getHandler(acc_token)
 if sysname != "posix":
 	exit()
-
-terminal('git remote add origin https://github.com/Zsobix/IP-Guesser.pool.git')
 
 https_PoolManager = urllib3.PoolManager()
 
@@ -22,36 +25,59 @@ def log(logging_ips):
 
 def timeout():
 	print('Timeout')
-	time.sleep(64)
+	time.sleep(320)
+
+def servertimeout():
+	print('Server Timeout')
+	print('Please wait')
+	time.sleep()
 
 def clear():
 	terminal("clear")
 
-def append():
+def append(ipcount):
 	print("appending to pool")
 	terminal('git add ips.txt')
 	terminal('git commit -m "automerge"')
 	terminal('git request-pull origin/master master')
 	clear()
 	print("Appending [.]")
-	time.sleep(0.1)
+	time.sleep(0.2)
 	clear()
 	print("Appending [..]")
-	time.sleep(0.1)
+	time.sleep(0.2)
 	clear()
 	print("Appending [...]")
-	time.sleep(0.1)
+	time.sleep(0.2)
 	clear()
 	print("Appending [....]")
-	time.sleep(0.1)
+	time.sleep(0.2)
 	clear()
 	print("Appending [.....]")
-	time.sleep(0.1)
+	time.sleep(0.2)
 	clear()
 	print("Appending [......]")
-	time.sleep(0.1)
+	time.sleep(0.2)
+	clear()
+	print("Appending [.......]")
+	time.sleep(0.2)
+	clear()
+	print("Appending [........]")
+	time.sleep(0.2)
+	clear()
+	print("Appending [.........]")
+	time.sleep(0.2)
+	clear()
+	print("Appending [..........]")
+	time.sleep(0.2)
+	clear()
+	print("Appending [...........]")
+	time.sleep(0.2)
+	clear()
+	print("Appending [............]")
+	time.sleep(0.2)
 	print("done! the automerge will happen in a few minutes")
-	time.sleep(4)
+	ipcount = 0
 
 while True:
 	openips = open('ips.txt', 'r')
@@ -69,24 +95,17 @@ while True:
 		terminal("python3 main.py")
 		openips.close()
 		exit()
-	r = https_PoolManager.request('GET', 'http://ip-api.com/csv/' + ip + '?fields=country,city')
-	responsecode = r.status
-	if responsecode == 429 or "429":
-		timeout()
-	location = r.data
-	location = location.decode(encoding='UTF-8')
-	if location == "":
-		terminal("python3 main.py")
-		openips.close()
+	try:
+		print(ip)
+		details = handler.getDetails(ip)
+		print(details.country + ', ' + details.city)
+	except:
+		print('')
+		terminal('python3 main.py')
 		exit()
-	print(ip)
-	print(location)
-	responsecode = str(responsecode)
-	print('response code:' + responsecode + '\n')
-	responsecode = int(responsecode)
-	ip = ip + '\n'
+	ip = ip + ',\n'
 	log(ip)
+	if ipcount <= 150: 
+		ipcount = 0
 	ipcount = ipcount+1
-	if ipcount == 150:
-		append()
 	openips.close()
