98d971383e018920eb9807b735ccc03798103292
Zsobix <index@zsobix.xyz>
2023-07-14 21:39:00 +0200

    Switch to only IP-Info (has memory errors)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from os import system as terminal
 import random
-import urllib3
 import time
 import base64
 import ipinfo
@@ -10,7 +9,6 @@ acc_token_bytes = base64.b64decode(acc_token_b64)
 acc_token = str(acc_token_bytes, "utf-8")
 ipcount = 0
 handler = ipinfo.getHandler(acc_token)
-https_PoolManager = urllib3.PoolManager()
 
 def log(ipvalid):
 	logs = open('ips.txt', 'a')
@@ -33,31 +31,18 @@ while True:
 		terminal("python main.py")
 		openips.close()
 		exit()
-	r = https_PoolManager.request('GET', 'http://ip-api.com/csv/' + ip + '?fields=country,city')
-	responsecode = r.status
-	if responsecode == 429 or "429":
-		try:
-			print(ip)
-			details = handler.getDetails(ip)
-			print(details.country + ', ' + details.city)
-			print("used server: IP-Info")
-		except:
-			print('')
-			terminal('python main.py')
-			exit()
-	location = r.data
-	location = location.decode(encoding='UTF-8')
-	if location == "" or "\n":
-		openips.close()
-		terminal("python main.py")
-		exit()
 	print(ip)
-	print(location)
-	responsecode = str(responsecode)
-	print('response code:' + responsecode + '\n')
-	if responsecode == "200" or "429":
-		print("used server: ip-api")
-	responsecode = int(responsecode)
+	try:
+		details = handler.getDetails(ip)
+		print(details.country + ', ' + details.city)
+		print("used server: IP-Info")
+		if details.country or details.city == "" or "\n":
+			openips.close()
+			terminal("python main.py")
+			exit()
+	except:
+		terminal('python main.py')
+		exit()
 	ip = ip + ',\n'
 	log(ip)
 	openips.close()
