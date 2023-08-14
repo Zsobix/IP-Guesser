5e25c9dd44e00e0335da7b39d217a8a5ebe96d75
Zsobix <index@zsobix.xyz>
2023-08-14 11:58:12 +0200

    Optimizing and rewriting auth system

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from os import system as terminal
 import random
+import urllib3
 import time
 import base64
 import ipinfo
+import os
 
-acc_token_b64 = "ODgyZmIwYTNjNTIwZTQ="
-acc_token_bytes = base64.b64decode(acc_token_b64)
-acc_token = str(acc_token_bytes, "utf-8")
+
+
+acc_token_input = input("(If you had an old session press ENTER)\n" +"What is your IP-Info API key?\n" + "If you don't have one please register at https://ipinfo.io/ \n" + "API key: ")
+
+try:
+	session = open("session.token", "r")
+	acc_token_input = session.read()
+
+except:
+	session = open("session.token", "w")
+	session_write = session.write(acc_token_input)
+
+acc_token = acc_token_input
 ipcount = 0
 handler = ipinfo.getHandler(acc_token)
 
@@ -28,21 +40,30 @@ while True:
 	ipnum4 = str(ipnum4)
 	ip = ipnum1 + "." + ipnum2 + "." + ipnum3 + "." + ipnum4
 	if ip in openips2:
-		terminal("python main.py")
 		openips.close()
-		exit()
-	print(ip)
-	try:
-		details = handler.getDetails(ip)
-		print(details.country + ', ' + details.city)
-		print("used server: IP-Info")
-		if details.country or details.city == "" or "\n":
+		continue
+	r = https_PoolManager.request('GET', 'http://ip-api.com/csv/' + ip + '?fields=country,city')
+	responsecode = r.status
+	if responsecode == 429 or "429":
+		try:
+			print(ip)
+			details = handler.getDetails(ip)
+			print(details.country + ', ' + details.city)
+			print("used server: IP-Info")
+		except:
 			openips.close()
-			terminal("python main.py")
-			exit()
-	except:
-		terminal('python main.py')
-		exit()
+			continue
+	location = r.data
+	location = location.decode(encoding='UTF-8')
+	if location == "" or "\n":
+		openips.close()
+		continue
+	print(location)
+	responsecode = str(responsecode)
+	print('response code:' + responsecode + '\n')
+	if responsecode == "200" or "429":
+		print("used server: ip-api")
+	responsecode = int(responsecode)
 	ip = ip + ',\n'
 	log(ip)
 	openips.close()
